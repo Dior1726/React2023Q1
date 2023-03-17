907259d69629ef2506a1605cf96b7afb35874b89
Dior1726 <muxammad_909@mail.ru>
2023-03-17 17:28:01 +0600

    feat: add basic style for search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import SearchBar from '../components/SearchBar';
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <div className="container">
         <h3 className="search-bar__title">RS School Powered Search</h3>
         <SearchBar />
