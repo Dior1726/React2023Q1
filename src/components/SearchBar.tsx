5e809ef7dfe5b779778df0291e59c3cee35693d8
Dior1726 <muxammad_909@mail.ru>
2023-03-18 18:16:02 +0600

    refactor: Searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const SearchBar = () => {
   return (
-    <div className="search-bar">
-      <input type="text" className="search-bar__input" />
-      <button className="search-bar__btn">Search</button>
+    <div className="flex flex-col gap-4">
+      <input
+        type="text"
+        className="outline-none rounded-sm p-2 text-gray-800 font-medium bg-white bg-opacity-80"
+        placeholder="Type..."
+      />
+      <button className="py-2 px-4 bg-gray-800 text-white rounded-sm hover:bg-gray-900 transition-all transform active:scale-95">
+        Search
+      </button>
     </div>
   );
 };
