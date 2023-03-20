10dbc29d270bef0b52ac61cf058b9aa9220cd930
Dior1726 <muxammad_909@mail.ru>
2023-03-20 14:22:47 +0600

    feat: add localstorage save function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const Home = () => {
+  const [searchList, setSearchList] = useState([]);
+  const [storedVal, setStoredVal] = useLocalStorage<string[]>('searchList', searchList);
+
+  useEffect(() => {
+    setSearchList(storedVal);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    setStoredVal(searchList);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchList]);
+
   return (
     <div className="flex-grow flex justify-center items-center flex-col ">
       <div className="bg-white bg-opacity-20 p-20 rounded-2xl">
         <h3 className="mb-10 text-center font-semibold text-lg">RS School Powered Search</h3>
-        <SearchBar />
+        <SearchBar setSearchList={setSearchList} />
+
+        <div className="mt-4">
+          {Array.isArray(searchList)
+            ? searchList.map((item, n) => (
+                <div className="border-b py-1 border-white border-opacity-20" key={n}>
+                  {item}
+                </div>
+              ))
+            : null}
+        </div>
       </div>
     </div>
   );
