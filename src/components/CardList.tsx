11bf75cd405399156c1807523666fdbaf56f72bf
Dior1726 <muxammad_909@mail.ru>
2023-03-23 18:38:07 +0600

    feat: done card list

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import Card from './Card';
 
-class CardList extends Component {
-  constructor(props: Record<string, never>) {
+interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+}
+
+interface IProducts {
+  products: IProduct[];
+}
+
+class CardList extends Component<IProducts, IProducts> {
+  constructor(props: IProducts) {
     super(props);
+
+    this.state = Object.assign({}, props);
+  }
+
+  card(product: IProduct) {
+    return (
+      <article
+        className="p-4 bg-white bg-opacity-20 rounded-2xl flex flex-col justify-between"
+        key={product.id}
+      >
+        <header>
+          <h4 className="font-bold text-xl mb-4">{product.title}</h4>
+          <p className="text-gray-300 mb-4"> {product.description}</p>
+        </header>
+        <main>
+          <img
+            className="w-full h-[200px] object-cover"
+            src={product.thumbnail}
+            alt={product.title}
+          />
+        </main>
+      </article>
+    );
   }
 
   render() {
     return (
-      <div>
-        <Card />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {this.state.products.map((product) => this.card(product))}
       </div>
     );
   }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar';
+import CardList from '../components/CardList';
+import { products } from '../data';
 
 class Home extends Component {
   constructor(props: Record<string, never>) {
@@ -9,10 +11,12 @@ class Home extends Component {
   render() {
     return (
       <div className="flex-grow flex-col ">
-        <div className="bg-white bg-opacity-20 p-20 rounded-2xl max-w-2xl w-full mx-auto">
+        <div className="bg-white mb-10 bg-opacity-20 p-20 rounded-2xl max-w-2xl w-full mx-auto">
           <h3 className="mb-10 text-center font-semibold text-lg">RS School Powered Search</h3>
           <SearchBar />
         </div>
+
+        <CardList products={products} />
       </div>
     );
   }
